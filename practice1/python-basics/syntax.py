838f5f33a212434d9ce43f9d03b878ce16aa73a9
k83610842-jpg <k83610842@gmail.com>
2026-01-27 14:06:01 +0500

    Add files via upload

---
+++ practice1/python-basics/syntax.py
@@ -0,0 +1,12 @@
+if 10>2:
+          print("10 is more than 2")
+#2
+if 90<10:
+        print("90 is less than 10")
+#3 variables
+a='Calculus 2'
+b="calc 4"
+g=503
+#-comments
+#4
+print("ABC"); print("Gold Apple"); print("Iphone")
