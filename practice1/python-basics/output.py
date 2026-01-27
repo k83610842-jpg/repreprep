e78967892d279a415c6de9134c4035495472463b
k83610842-jpg <k83610842@gmail.com>
2026-01-27 23:56:58 +0500

    Add files via upload

---
+++ practice1/python-basics/output.py
@@ -0,0 +1,15 @@
+#1
+print("Book book")
+print("kapets")
+#2
+print('lada')
+print("lada")
+#3
+print("Salem",end=" ")
+print("vsem")
+#4
+print(994802940)
+print(89+2)
+print(30*6)
+#5 
+print("My dog is", 5,"years old")
