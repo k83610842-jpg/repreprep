9229fe8d69b92d6b173a662a7528c0d7dd1fee77
k83610842-jpg <k83610842@gmail.com>
2026-01-27 23:57:39 +0500

    Add files via upload

---
+++ practice1/python-basics/variables.py
@@ -0,0 +1,84 @@
+#1
+a=45
+b="Sam"
+print(b)
+print(a)
+#2
+f=932
+f='Asyl'
+print(f)
+#3
+a=str(404) #'404'
+a=int(404) #404
+a=float(404) #404.0
+#4
+a=3.4
+b="Kuralai"
+print(type(a))
+print(type(b))
+#5
+print('BMW')
+print("BMW")
+#6
+a=30
+A='kapets' #a and A are two different variables
+#7
+NUMBER=30
+number1=30
+nuMber=30
+hisFirstScore=50
+HisFirstScore=50
+his_first_score=50
+#8
+a,b,c=70,20,"Cat"
+print(a,b,c)
+#9
+a=b=c='pdf'
+print(a,b,c)
+#10
+apples=['green','red','mixed']
+a,b,c=apples
+print(a)
+print(b)
+print(c)
+#11
+a='gold'
+b='apple'
+print(a,b)
+a='gold '
+b='apple'
+print(a+b)
+#12
+a=5
+b=3023
+print(a+b)
+#13
+a='dog number'
+b=1
+print(a,b)
+#14
+a=23
+def plus():
+    print(45+a)
+plus()
+#15
+a='green'
+def color():
+    a='orange'
+    print('the room is',a)
+color()
+print('the room is', a)
+#16
+def kbtu():
+    global a
+    a='chill'
+kbtu()
+print(a,"guy")
+#17
+a='truth'
+def cd():
+    global a
+    a='false'
+cd()
+print('the statement is',a)
+
