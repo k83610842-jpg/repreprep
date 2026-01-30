c4dc15ca8850d2464fa848a73b2e59f3ed1c0ba0
k83610842-jpg <k83610842@gmail.com>
2026-01-30 23:36:47 +0500

    Update dataTypes.py

--- practice1/python-basics/dataTypes.py
+++ practice1/python-basics/dataTypes.py
@@ -2,12 +2,19 @@
 a='cat'
 print(type(a))
 #2
+a=76
+print(type(a))
+#3
 a="London Time"
 a=84983
 a=332.33
 a=True
 a=range(6)
-#3
+#4
 a=float(332.33)
 a=str("London Time")
 a=int(94394)
+#5
+a=5345.345
+print(type(a))
+
