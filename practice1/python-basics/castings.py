9a288b619b47972fb88a9449d7da796c315a1bc6
k83610842-jpg <k83610842@gmail.com>
2026-01-30 20:31:26 +0500

    Add files via upload

---
+++ practice1/python-basics/castings.py
@@ -0,0 +1,13 @@
+#11
+a = int(44)  
+b = int(4.4) 
+c = int("4") 
+#12
+a = float(4)     # x will be 1.0
+b = float(4.4)   # y will be 2.8
+c = float("4")   # z will be 3.0
+d = float("4.4") # w will be 4.2
+#13
+x = str("data") # x will be 's1'
+y = str(4)    # y will be '2'
+z = str(4.4)  # z will be '3.0'
