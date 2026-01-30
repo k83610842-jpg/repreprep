ae5c3f322daff30befb27bc6abdd07a8e4fb3c75
k83610842-jpg <k83610842@gmail.com>
2026-01-30 23:34:59 +0500

    Update castings.py

--- practice1/python-basics/castings.py
+++ practice1/python-basics/castings.py
@@ -3,11 +3,12 @@ a = int(44)
 b = int(4.4) 
 c = int("4") 
 #12
-a = float(4)     # x will be 1.0
-b = float(4.4)   # y will be 2.8
-c = float("4")   # z will be 3.0
-d = float("4.4") # w will be 4.2
+a = float(4)      
+b = float(4.4)   
+c = float("4")  
+d = float("4.4")
 #13
-x = str("data") # x will be 's1'
-y = str(4)    # y will be '2'
-z = str(4.4)  # z will be '3.0'
+x = str("data") 
+y = str(4)   
+
+z = str(4.4) 
