eee55bcbf23fea5c941f700c069693ea733ed864
k83610842-jpg <k83610842@gmail.com>
2026-01-30 20:30:50 +0500

    Add files via upload

---
+++ practice1/python-basics/numbers.py
@@ -0,0 +1,27 @@
+#4
+a=32
+a=4.3
+a=1j
+#5
+a=48930923
+a=-874874983
+a=3
+#6
+a=932.231
+a=3.2
+a=-3434.4
+#7
+a=4e7
+a=930E3
+a=-23.23e5
+#8
+a= 4-3j
+b = 76j
+f = -90j
+#9
+x=4
+c=float(x)
+d=complex(x)
+#10
+import random
+print(random.randrange(1,12))
